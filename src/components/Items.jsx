dfa22b11ac97413adc6f8e7318dd7f9503b69389
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-01-08 13:30:59 +0530

    Application complete with all the functionalities except localStorage, optimization needed.

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 
 const Items = ({
   item,
   handleDelete,
+  isEditEnabled,
   handleEdit,
   checkedItem,
   handleCheckItem,
 }) => {
+  const editInputVal = useRef();
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleEdit(item, editInputVal.current.value);
+      editInputVal.current.value = "";
+    }
+  };
+
   return (
-    <li className="flex justify-between items-center bg-slate-800 bg-opacity-30 hover:shadow-xl p-3 rounded-full min-w-80 transition-all ease-in-out hover:scale-105">
-      <input
-        type="checkbox"
-        name="todo"
-        id={item}
-        className="checked:before:flex checked:before:justify-center checked:before:items-center checked:before:content-['\2713'] bg-transparent border rounded-full w-4 h-4 checked:before:text-xs transition-all cursor-pointer appearance-none checked:scale-105 ease-in-out"
-        onChange={(e) => handleCheckItem(e)}
-      />
-      <span>{item}</span>
+    <li className="flex justify-between items-center gap-2 bg-slate-800 bg-opacity-30 hover:shadow-xl p-3 rounded-full min-w-96 text-xl transition-all ease-in-out hover:scale-105">
+      {isEditEnabled === item ? (
+        <input
+          type="text"
+          className="bg-transparent bg-opacity-30 mx-4 pl-5 w-48 text-white transition-all caret-color-200 ease-in-out outline-none placeholder-slate-400"
+          ref={editInputVal}
+          placeholder={item}
+          onKeyUp={(e) => handleEnter(e)}
+        />
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            name="todo"
+            id={item}
+            className="checked:before:flex checked:before:justify-center checked:before:items-center checked:before:content-['\2713'] bg-transparent checked:bg-orange-500 border rounded-full w-4 h-4 checked:before:text-xs placeholder:text-neutral-300 transition-all cursor-pointer appearance-none checked:scale-105 ease-in-out"
+            onChange={(e) => handleCheckItem(e)}
+          />
+          <p
+            className={
+              checkedItem.includes(item)
+                ? "line-through decoration-orange-500 break-words text-wrap max-w-[50vw]"
+                : "break-words text-wrap max-w-[50vw]"
+            }
+          >
+            {item}
+          </p>
+        </>
+      )}
+
       <div>
+        {!isEditEnabled && (
+          <button
+            className="bg-yellow-500 mx-1 p-2 rounded-full transition ease-in-out hover:scale-110 active:scale-95"
+            onClick={() => handleEdit(item, null)}
+          >
+            <MdEdit />
+          </button>
+        )}
+
         <button
-          className="bg-yellow-500 mx-1 p-2 rounded-full transition ease-in-out hover:scale-110 active:scale-95"
-          onClick={() => handleEdit(item)}
-        >
-          <MdEdit />
-        </button>
-        <button
-          className="bg-red-500 mx-1 p-2 rounded-full transition ease-in-out hover:scale-110 active:scale-95"
+          className="bg-red-500 m-1 p-2 rounded-full transition ease-in-out hover:scale-110 active:scale-95"
           onClick={() => handleDelete(item)}
         >
           <MdDelete />
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -5,6 +5,7 @@ import Items from "./Items";
 const TodoContainer = () => {
   const [items, setItems] = useState([]);
   const [checkedItem, setCheckedItem] = useState([]);
+  const [isEditEnabled, setIsEditEnabled] = useState(null);
   const inputVal = useRef();
 
   const handleAddItems = (item) => {
@@ -24,28 +25,35 @@ const TodoContainer = () => {
 
   const handleDelete = (item) => {
     setItems((prev) => prev.filter((i) => i !== item));
+    setCheckedItem((prev) => prev.filter((i) => i !== item));
+    setIsEditEnabled(null);
   };
 
-  const handleEdit = (item) => {
-    console.log("Edit", item);
-  };
-
-  const handleCheckItem = (e) => {
-    console.log(e.target.id);
-    console.log(e.target.checked);
-    if (!checkedItem.includes(e.target.id)) {
-      setCheckedItem((prev) => [...prev, e.target.id]);
+  const handleEdit = (item, editInputValue) => {
+    if (!isEditEnabled) {
+      setIsEditEnabled(item);
     } else {
-      setCheckedItem((prev) => prev.filter((i) => i !== item));
+      setIsEditEnabled(null);
+      console.log(items);
+      if (editInputValue && editInputValue !== "") {
+        setItems((prev) => prev.map((i) => (i === item ? editInputValue : i)));
+        if (checkedItem.includes(item)) {
+          setCheckedItem((prev) => prev.filter((i) => i !== item));
+        }
+      }
     }
   };
 
-  useEffect(() => {
-    console.log(items);
-  }, [items]);
+  const handleCheckItem = (e) => {
+    if (!checkedItem.includes(e.target.id)) {
+      setCheckedItem((prev) => [...prev, e.target.id]);
+    } else {
+      setCheckedItem((prev) => prev.filter((i) => i !== e.target.id));
+    }
+  };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-5 shadow-2xl backdrop-blur-2xl p-4 border border-opacity-50 rounded-2xl min-w-96 min-h-96 text-center">
+    <div className="flex flex-col justify-center items-center gap-5 shadow-2xl backdrop-blur-2xl p-4 border border-opacity-50 rounded-2xl w-[80vw] min-w-96 min-h-96 text-center">
       <h1 className="font-bold text-3xl text-white">To Do - React App</h1>
       <div className="flex justify-between items-center w-full">
         <input
@@ -53,7 +61,7 @@ const TodoContainer = () => {
           placeholder="Enter To Do item..."
           className="bg-slate-800 bg-opacity-30 hover:shadow-xl ml-4 p-3 pl-5 rounded-full w-full text-white transition-all caret-color-200 ease-in-out outline-none placeholder-neutral-200"
           ref={inputVal}
-          onKeyDown={(e) => handleEnter(e)}
+          onKeyUp={(e) => handleEnter(e)}
         />
         <button
           className="bg-slate-800 bg-opacity-30 hover:shadow-xl m-2 p-3 rounded-full text-white text-xl transition-all cursor-pointer ease-in-out hover:scale-105 active:scale-95"
@@ -64,11 +72,7 @@ const TodoContainer = () => {
       </div>
       <ul className="flex flex-col justify-center items-center gap-3 font-semibold text-white">
         {items.length === 0 ? (
-          <img
-            src="/public/assets/no-items.png"
-            alt="no-items"
-            className="w-40"
-          />
+          <img src="/assets/no-items.png" alt="no-items" className="w-40" />
         ) : (
           items.map((item, index) => {
             return (
@@ -76,6 +80,7 @@ const TodoContainer = () => {
                 key={item + index}
                 item={item}
                 handleDelete={handleDelete}
+                isEditEnabled={isEditEnabled}
                 handleEdit={handleEdit}
                 checkedItem={checkedItem}
                 handleCheckItem={handleCheckItem}
